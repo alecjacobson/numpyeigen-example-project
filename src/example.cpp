3d0de73124a35299aa3b41dde910149eec0e3fd3
Alec Jacobson <alecjacobson@adobe.com>
2023-09-25 09:23:48 -0400

    minimal reproducible example

--- src/example.cpp
+++ src/example.cpp
@@ -6,13 +6,9 @@ const char* example_function_doc = R"igl_Qu8mg5v7(
 Simple example function that returns its input
 )igl_Qu8mg5v7";
 npe_function(example_function)
-npe_arg(in1, dense_int32, dense_int64)
-npe_arg(in2, npe_matches(in1))
 npe_doc(example_function_doc)
+npe_arg(A_cpp, dense_float, dense_double)
+npe_arg(B_cpp, npe_matches(A_cpp))
 npe_begin_code()
-{
-    std::cout << "sizeof(in1[0])*8: " << sizeof(in1[0])*8 << std::endl;
-    std::cout << "sizeof(in2[0])*8: " << sizeof(in2[0])*8 << std::endl;
-    return std::make_tuple(npe::move(in1), npe::move(in2));
-}
+std::cout<<"B_cpp:\n"<<B_cpp<<std::endl;
 npe_end_code()
--- tests/test_example.py
+++ tests/test_example.py
@@ -1,39 +1,7 @@
-from __future__ import print_function
-import unittest
-import os
+from example import example_function
+import numpy as np
 
-
-class TestExample(unittest.TestCase):
-    def setUp(self):
-        self.test_path = os.path.join(os.path.dirname(
-            os.path.realpath(__file__)), "..", "data")
-
-    def test_example(self):
-        from example import example_function
-        import numpy as np
-
-        # array of 100 random int32 numbers
-        a1 = np.random.randint(0, 100, 100).astype(np.int32)
-        a2 = np.random.randint(0, 100, 100).astype(np.int32)
-        b1, b2 = example_function(a1, a2)
-        self.assertTrue(np.all(a1 == b1))
-        self.assertTrue(np.all(a2 == b2))
-
-        # array of 100 random int64 numbers
-        a1 = np.random.randint(0, 100, 100).astype(np.int64)
-        a2 = np.random.randint(0, 100, 100).astype(np.int64)
-        b1, b2 = example_function(a1, a2)
-        self.assertTrue(np.all(a1 == b1))
-        self.assertTrue(np.all(a2 == b2))
-
-        for dtype_str in ["intc", "int", "long", "longlong"]:
-            a1 = np.random.randint(0, 100, 100,dtype=dtype_str)
-            a2 = np.random.randint(0, 100, 100,dtype=dtype_str)
-            b1, b2 = example_function(a1, a2)
-            self.assertTrue(np.all(a1 == b1))
-            self.assertTrue(np.all(a2 == b2))
-
-
-
-if __name__ == '__main__':
-    unittest.main()
+A_python = np.array([0.5, 0.1, 10.])
+B_python = np.array([[0., 1., 2.],[3., 4., 5.]])
+print("B_python:\n", B_python)
+example_function(A_python,B_python)
